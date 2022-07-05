206aa680aaf97f41fbe191f239d6e00719904990
Patrick <patrick@godknows.de>
2022-07-05 11:55:28 +0200

    Add project files.

---
+++ BlazorGoogleAuthTutorial/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using BlazorGoogleAuthTutorial.Server.Models;
+using Duende.IdentityServer.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace BlazorGoogleAuthTutorial.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
---
+++ BlazorGoogleAuthTutorial/Server/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BlazorGoogleAuthTutorial.Server.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
